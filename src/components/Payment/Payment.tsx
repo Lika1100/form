26c98dec0e0131018ff1f571b366e2b9f5cd14ab
Lika1100 <akoesnikova@gmail.com>
2024-05-16 13:40:37 +0300

    payment adaptive and emulation

--- src/components/Payment/Payment.tsx
+++ src/components/Payment/Payment.tsx
@@ -1,26 +1,74 @@
+import cn from "classnames";
+import { observer } from 'mobx-react-lite';
 import * as React from 'react';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from 'components/Button';
 import Input from 'components/Input';
+import Loader from 'components/Loader';
 import Text from 'components/Text';
-import { statusAuth } from 'store/RootStore/AuthStore/AuthStore';
 import rootStore from 'store/RootStore/instance';
 import styles from "./Payment.module.scss";
 
-
-export default function Payment() {
+function Payment() {
     const [cardNum, setCardNum] = useState("")
     const [cardMonth, setCardMonth] = useState("")
     const [cardYear, setCardYear] = useState("")
     const [cardCvc, setCardCvc] = useState("")
+    const [isLoading, setIsLoading] = useState(false)
+    const [alertMessage, setAlertMessage] = useState({
+        text: "",
+        show: "hidden"
+    })
+    const navigate = useNavigate()
 
     const total = rootStore.cart.cart
         .map(({ price, count }) => price !== null ? price * count : 0)
         .reduce((acc, prev) => acc + prev, 0)
+    
 
-    const isAuth = rootStore.user.authStatus === statusAuth.auth
+    const isAuth = rootStore.user.isAuthorized
+
+    function onPay() {
+        setTimeout(() => {
+            setIsLoading(false)
+            setAlertMessage((prev) => {
+                prev.show = "show"
+                prev.text = "Your payment is success"
+                return prev
+            })            
+        }, 3000)
+        setIsLoading(true)
+    }
+
+    function onClick() {
+        setAlertMessage(() => {
+            return { ...alertMessage, show: "hidden" }
+        })
+        navigate("/")
+    }
+    if (isLoading) {
+        return (
+            <div className={styles.paymentLoader}>
+                <Text>Please, wait. We check your payment</Text>
+                <Loader size="l"/>
+            </div>
+        )
+    }
+
     return (
         <div className={styles.wrap}>
+            <div className={cn(styles.wrapMessage, styles[alertMessage.show])}>
+                <Text view="p-20" className={styles.wrapMessageText}>
+                    {alertMessage.text}
+                </Text>
+                <Button
+                    className={styles.wrapMessageButton}
+                    onClick={onClick}
+                >
+                    OK
+                </Button>
+            </div>
             <form
                 onSubmit={(e) => e.preventDefault()}
                 className={styles.payment}
@@ -59,8 +107,14 @@ export default function Payment() {
                         />
                     </div>
                 </div>
-                <Button className={styles.paymentButton} disabled={!isAuth}>Оплатить {total === 0 ? "" : `${total}$`}</Button>
+                <Button 
+                  className={styles.paymentButton} 
+                  disabled={!isAuth || alertMessage.show === "show" } onClick={onPay}>
+                    Pay {total === 0 ? "" : `${total}$`}
+                </Button>
             </form>
         </div>
     )
 }
+
+export default observer(Payment)
